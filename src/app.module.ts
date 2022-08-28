150fd0bdab3af0b9426953c3390efad102115f92
JayMtz <jxspell@gmail.com>
2022-08-28 13:11:55 -0500

    created new history objects
    populated a history module/controller/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [],
+  imports: [HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
